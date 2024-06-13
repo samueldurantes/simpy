a94c9d76d967cc9e4afda9d85294f3789d81a494
Samuel Durante <samueldurante.h@gmail.com>
2024-06-13 23:56:32 +0000

    Initial commit

---
+++ Makefile
@@ -0,0 +1,4 @@
+.PHONY: run
+run:
+	@echo "Running the program..."
+	flask --app src/main.py run
---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs;
+            let
+              pythonWithPackages = python3.withPackages (p:
+                with p; [
+                  flask
+                ]
+              );
+            in
+            [
+              pythonWithPackages
+              gnumake
+            ];
+        };
+      }
+    );
+}
