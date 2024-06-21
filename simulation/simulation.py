68bc4f7aed35f9b656b52b8148590172e615af2e
= <tiago.panizio@hotmail.com>
2024-06-21 19:35:50 -0300

    set support to bradesco bank

--- simulation/simulation.py
+++ simulation/simulation.py
@@ -1,7 +1,10 @@
 from enum import Enum
 from simulation.simulators.itau import Itau
+from simulation.bradesco.bradesco import Bradesco
 
-SupportedBanks = Enum('Banks', ['ITAU'])
+class SupportedBanks(Enum):
+    ITAU = Itau
+    BRADESCO = Bradesco
 
 class Simulation:
   def __init__(self, bank: str, financing_value: float, installments_number: int, age: int):
@@ -11,21 +14,20 @@ class Simulation:
     self.age = age
 
   def run(self):
-    if self.bank == SupportedBanks.ITAU.name:
-      itau = Itau(self.financing_value, self.installments_number, self.age, "Personnalité")
+      try:
+        bank = SupportedBanks[self.bank].value(self.financing_value, self.installments_number, self.age, "Personnalité")
+      except ValueError:
+        return {"error": str(f"Bank '{self.bank}' is not supported.")}, 400
 
       try:
-          i = itau.simulate_all_installments()
+          i = bank.simulate_all_installments()
           installments = list(map(lambda i: {"installment": i[0], "value": i[1]}, i))
 
           return {
-              "bank": self.bank,
-              "financing_value": self.financing_value,
+              "bank": self.bank, "financing_value": self.financing_value,
               "installments_number": self.installments_number,
               "age": self.age,
               "installments": installments,
           }, 200
       except ValueError as error:
           return {"error": str(error)}, 400
-    else:
-      return {"error": "Bank not supported"}, 400
