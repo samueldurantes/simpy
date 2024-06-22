25348ce49774f0e59915bc112e5a41255f60f9d2
= <tiago.panizio@hotmail.com>
2024-06-22 11:24:57 -0300

    refactor supported banks enum

--- simulation/simulation.py
+++ simulation/simulation.py
@@ -1,10 +1,4 @@
-from enum import Enum
-from simulation.simulators.itau import Itau
-from simulation.bradesco.bradesco import Bradesco
-
-class SupportedBanks(Enum):
-    ITAU = Itau
-    BRADESCO = Bradesco
+from simulation.banks.banks import Banks
 
 class Simulation:
   def __init__(self, bank: str, financing_value: float, installments_number: int, age: int):
@@ -15,19 +9,19 @@ class Simulation:
 
   def run(self):
       try:
-        bank = SupportedBanks[self.bank].value(self.financing_value, self.installments_number, self.age, "Personnalité")
+        bank = Banks[self.bank].value
       except ValueError:
         return {"error": str(f"Bank '{self.bank}' is not supported.")}, 400
 
       try:
-          i = bank.simulate_all_installments()
-          installments = list(map(lambda i: {"installment": i[0], "value": i[1]}, i))
+        i = bank.simulate_all_installments(self.financing_value, self.installments_number, self.age)
+        installments = list(map(lambda i: {"installment": i[0], "value": i[1]}, i))
 
-          return {
-              "bank": self.bank, "financing_value": self.financing_value,
-              "installments_number": self.installments_number,
-              "age": self.age,
-              "installments": installments,
-          }, 200
+        return {
+            "bank": self.bank, "financing_value": self.financing_value,
+            "installments_number": self.installments_number,
+            "age": self.age,
+            "installments": installments,
+        }, 200
       except ValueError as error:
-          return {"error": str(error)}, 400
+        return {"error": str(error)}, 400
